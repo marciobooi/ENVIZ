3a1dd5681f4667aca96d39fce26a6d589f56b239
marcio <78marcio@gmail.com>
2025-01-22 22:12:11 +0100

    fix css styles

--- enviz/src/App.jsx
+++ enviz/src/App.jsx
@@ -4,14 +4,12 @@ import CardsContainer from './components/CardsContainer';
 import Tutorial from './components/Tutorial';
 import TutorialProvider from './contexts/TutorialContext';
 import Footer from './components/Footer';
-import '@ecl/preset-eu/dist/styles/optional/ecl-reset.css';
-import '@ecl/preset-eu/dist/styles/optional/ecl-eu-default.css';
-import '@ecl/preset-eu/dist/styles/ecl-eu.css';
-import '@ecl/preset-eu/dist/styles/optional/ecl-eu-utilities.css';
-import '@ecl/preset-eu/dist/scripts/ecl-eu.js';
+
+// import '@ecl/preset-eu/dist/scripts/ecl-eu.js';
 import Enprices from './components/Enprices';
 import getCardData from './config/cardData';
 import { useTranslation } from 'react-i18next';
+
 
 function App() {
   const { t } = useTranslation();
--- enviz/src/main.jsx
+++ enviz/src/main.jsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@ecl/preset-eu/dist/styles/optional/ecl-reset.css';
+import '@ecl/preset-eu/dist/styles/optional/ecl-eu-default.css';
+import '@ecl/preset-eu/dist/styles/ecl-eu.css';
+import '@ecl/preset-eu/dist/styles/optional/ecl-eu-utilities.css';
 
 import './index.css'
 import App from './App.jsx'
